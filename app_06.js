0b3a38a11a76a87bcd9543001c93d923ad151348
Local Admin Ruohoniemi Janne <lad_ruohoniemi_j@sairaala.local>
2020-01-16 20:29:04 +0200

    memo2

---
+++ app_06.js
@@ -0,0 +1,73 @@
+const http = require('http'); // http-moduuli
+const fs = require('fs'); // filestream
+
+const notes = [];
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    console.log(`HTTP request received: url=${url} method=${method}`)
+
+    if (url === '/') {
+        res.write(`
+        <html>
+        <head><title>MemoApp</title></head>
+        <body>`);
+
+        notes.forEach((value, index) => {
+            res.write(`<div>note: ${value}, index: ${index}</div>`);
+        });
+
+        res.write(`
+            <form action="add-note" method="POST">
+                <input type="text" name="note">
+                <button type="submit">Add note</button>
+            </form>
+        </body>
+        </html>
+        `);
+        res.statusCode = 200; // OK
+        res.end();
+        return;
+    }
+    else if (url === '/add-note') {
+        console.log('/add-note');
+        const chunks = [];
+        req.on('data', (chunk) => {
+            chunks.push(chunk);
+        });
+
+        req.on('end', () => {
+            const body = Buffer.concat(chunks).toString();
+            const note = body.split('=')[1];
+            notes.push(note);
+            
+            res.statusCode = 303; // See other, redirect
+            res.setHeader('Location', '/');
+            res.end();
+        });
+
+        return;
+    }
+    else if (url === '/favicon.ico') {
+        fs.readFile('./favicon.ico', (err, data) => {
+            res.write(data);
+            res.end();
+        });
+        return;
+    }
+
+    console.log(`${url} not found`);
+    res.write(`
+        <html>
+        <head><title>MemoApp - 404</title></head>
+        <body>
+            <h1>404 - page not found</h1>
+        </body>
+        </html>
+        `);
+    res.statusCode = 404; // Not found
+    res.end();
+});
+
+server.listen(8080);
